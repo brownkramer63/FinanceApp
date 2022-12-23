88908e3f674f7962629f866d2f9531a3828dcbbc
Adelina <adelina.abdykanyeva@gmail.com>
2022-12-23 15:10:49 -0600

    create InvoiceEntity class

---
+++ src/main/java/com/cydeo/entity/InvoiceEntity.java
@@ -0,0 +1,42 @@
+package com.cydeo.entity;
+
+
+import com.cydeo.enums.InvoiceStatus;
+import com.cydeo.enums.InvoiceType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "invoices")
+@Getter
+@Setter
+@NoArgsConstructor
+public class InvoiceEntity extends BaseEntity{
+
+
+    private String invoiceNo;
+    @Enumerated(EnumType.STRING)
+    private InvoiceStatus invoiceStatus;
+    @Enumerated(EnumType.STRING)
+    private InvoiceType invoiceType;
+    @Column(columnDefinition = "DATE")
+    private LocalDate date;
+    /*
+    Waiting for other group mates to complete
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ClientVendor clientVendor;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
+
+     */
+
+
+
+
+
+}
