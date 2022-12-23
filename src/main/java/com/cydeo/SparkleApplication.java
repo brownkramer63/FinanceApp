60d5a06db2b0f8f68217d34a95e5d39f1a3eb924
Baha Tastan <59250233+bahatas@users.noreply.github.com>
2022-12-23 13:57:17 +0000

    initial commit of sparkle team project

--- src/main/java/com/cydeo/SparkleApplication.java
+++ src/main/java/com/cydeo/SparkleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NumberOneApplication {
+public class SparkleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NumberOneApplication.class, args);
+        SpringApplication.run(SparkleApplication.class, args);
     }
 
 }
--- src/test/java/com/cydeo/SparkleApplicationTests.java
+++ src/test/java/com/cydeo/SparkleApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NumberOneApplicationTests {
+class SparkleApplicationTests {
 
     @Test
     void contextLoads() {
