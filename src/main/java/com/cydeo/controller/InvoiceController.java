95766d9dc5ae826000449ab751dd16d3664b3d14
Adelina <adelina.abdykanyeva@gmail.com>
2022-12-24 16:14:52 -0600

    [JDZH3-129]feat: create InvoiceController class

---
+++ src/main/java/com/cydeo/controller/InvoiceController.java
@@ -0,0 +1,50 @@
+package com.cydeo.controller;
+
+
+import com.cydeo.dto.InvoiceDTO;
+import com.cydeo.service.InvoiceService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/invoice")
+public class InvoiceController {
+
+
+    private final InvoiceService invoiceService;
+
+
+    public InvoiceController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/salesInvoices/list")
+    public String createSalesInvoice(Model model){
+        model.addAttribute("invoices", new InvoiceDTO());
+
+        return "/invoice/sales-invoice-list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteSalesInvoice(@PathVariable("id") Long id){
+        invoiceService.delete(id);
+        return "redirect:/invoice/salesInvoices/list";
+
+    }
+
+    @GetMapping("/salesInvoices/update/{id}")
+    public String editSalesInvoice(@PathVariable("id") Long id, Model model){
+
+        model.addAttribute();
+
+        return "/invoice/sales-invoice-update";
+    }
+
+
+
+
+
+}
