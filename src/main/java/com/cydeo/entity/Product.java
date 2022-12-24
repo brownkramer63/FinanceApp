f873e5854c71bb852f749695b1bd32c2b271a3e6
entisar08 <eelmohbat@gmail.com>
2022-12-24 00:52:51 -0600

    updating some fields

--- src/main/java/com/cydeo/entity/Product.java
+++ src/main/java/com/cydeo/entity/Product.java
@@ -1,14 +1,13 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.ProductUnit;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Getter
@@ -21,8 +20,10 @@ public class Product extends BaseEntity{
 
     private int lowLimitAlert;
     private String name;
-    private String productUnit;
+
+    @Enumerated(EnumType.STRING)
+    private ProductUnit productUnit;
     private int qtyInStock;
-    @ManyToOne
-    Category category;
+   // @ManyToOne
+   // Category category;
 }
