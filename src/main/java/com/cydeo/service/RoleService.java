b342abe302adaf04d194019c7bd0bf30529d24b3
Violetta <violetta.v.queen@gmail.com>
2022-12-24 07:04:04 -0700

    [JDZH3 -48] feat: role service findById added amd impl

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -1,4 +1,8 @@
 package com.cydeo.service;
 
+import com.cydeo.dto.RoleDTO;
+
 public interface RoleService {
+
+    RoleDTO findById(Long id);
 }
--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -1,10 +1,24 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.mapper.MapperUtil;
+import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
+    private final RoleRepository roleRepository;
+    private final MapperUtil mapperUtil;
 
+    public RoleServiceImpl(RoleRepository roleRepository, MapperUtil mapperUtil) {
+        this.roleRepository = roleRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+    @Override
+    public RoleDTO findById(Long id) {
+        return mapperUtil.convert(roleRepository.findById(id), new RoleDTO());
+    }
 }
