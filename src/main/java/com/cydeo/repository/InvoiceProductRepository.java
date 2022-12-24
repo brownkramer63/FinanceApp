86cfbc78307a6ca32eecbd444e460b2cd11cd7ee
Adelina <adelina.abdykanyeva@gmail.com>
2022-12-23 18:56:32 -0600

    create InvoiceProductRepository

---
+++ src/main/java/com/cydeo/repository/InvoiceProductRepository.java
@@ -0,0 +1,10 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.InvoiceProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceProductRepository extends JpaRepository<InvoiceProduct, Long> {
+
+    InvoiceProduct findByInvoiceId(Long id);
+
+}
