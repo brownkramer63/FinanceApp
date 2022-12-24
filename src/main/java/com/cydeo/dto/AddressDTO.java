c0b5b5c1c1322129791c6f2c24c18a070366dbb2
Kadifa <Kudickadifa@gmail.com>
2022-12-24 15:57:38 -0600

    [] feat : created AddressDTO

---
+++ src/main/java/com/cydeo/dto/AddressDTO.java
@@ -0,0 +1,13 @@
+package com.cydeo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressDTO {
+}
