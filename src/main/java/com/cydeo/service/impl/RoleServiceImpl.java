ef6a721000e195690351b6d092d9036296787c91
Violetta <violetta.v.queen@gmail.com>
2022-12-23 18:01:24 -0700

    user - RoleServiceImpl class is created

---
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.service.RoleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+
+}
