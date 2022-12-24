6a2abb5e88baae4871086401b9f62c39b7aba444
Adelina <adelina.abdykanyeva@gmail.com>
2022-12-23 18:57:56 -0600

    create Model mapper class

--- src/main/java/com/cydeo/SparkleApplication.java
+++ src/main/java/com/cydeo/SparkleApplication.java
@@ -1,13 +1,21 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SparkleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SparkleApplication.class, args);
+    }
+
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }
