84bf361005c2623fed1a0afd74f076f7db07e211
Kadifa <Kudickadifa@gmail.com>
2022-12-24 15:53:35 -0600

    [JDZH3-99] feat : created CategoryServiceImpl

---
+++ src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,27 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.CategoryDTO;
+import com.cydeo.entity.Category;
+import com.cydeo.mapper.MapperUtil;
+import com.cydeo.repository.CategoryRepository;
+import com.cydeo.service.CategoryService;
+
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    private final MapperUtil mapperUtil;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil) {
+        this.categoryRepository = categoryRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+
+    @Override
+    public CategoryDTO findById(Long Id) {
+
+        Category category = categoryRepository.findById(Id).get();
+        return  mapperUtil.convert(category , new CategoryDTO());
+    }
+}
