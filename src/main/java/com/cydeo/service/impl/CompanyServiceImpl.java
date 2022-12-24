36c08eed5135fbb21fe3e3693f03da168246838f
nihano <nioztekin@gmail.com>
2022-12-25 01:54:16 +0300

    JDZH3-54 feat: Add CompanyService interface and findById() implementation, fixed ModelMapper

--- src/main/java/com/cydeo/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/CompanyServiceImpl.java
@@ -2,30 +2,28 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.CompanyDTO;
 import com.cydeo.entity.Company;
-import com.cydeo.mapper.CompanyMapper;
+import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.CompanyRepository;
 import com.cydeo.service.CompanyService;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class CompanyServiceImpl implements CompanyService {
 
     private final CompanyRepository companyRepository;
-    private final CompanyMapper companyMapper;
+    private final MapperUtil mapperUtil;
 
-
-    public CompanyServiceImpl(CompanyRepository companyRepository, CompanyMapper companyMapper) {
+    public CompanyServiceImpl(CompanyRepository companyRepository, MapperUtil mapperUtil) {
         this.companyRepository = companyRepository;
-        this.companyMapper = companyMapper;
+        this.mapperUtil = mapperUtil;
     }
+
 
     @Override
     public CompanyDTO findById(Long Id) {
 
         Company company = companyRepository.findById(Id).get();
-        return companyMapper.convertToDTO(company);
+        return mapperUtil.convert(company, new CompanyDTO());
     }
 
 
