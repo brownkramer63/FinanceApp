09833a0c62c233f7479b5829ce89851ac3dc27ae
Adelina <adelina.abdykanyeva@gmail.com>
2022-12-24 16:17:31 -0600

    [JDZH3-113]feat: create InvoiceProduct Service

--- src/main/java/com/cydeo/service/InvoiceProductService.java
+++ src/main/java/com/cydeo/service/InvoiceProductService.java
@@ -1,10 +1,11 @@
 package com.cydeo.service;
 
+import com.cydeo.dto.InvoiceDTO;
 import com.cydeo.dto.InvoiceProductDTO;
 
 public interface InvoiceProductService {
 
-    void save(InvoiceProductDTO dto);
+    void save(InvoiceDTO dto);
     void update(InvoiceProductDTO dto);
     void delete(Long id);
 
