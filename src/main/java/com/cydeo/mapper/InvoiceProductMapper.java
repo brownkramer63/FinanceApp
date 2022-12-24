9f14eac3690f5ce13cc066c4239202ee8ea7818b
Adelina <adelina.abdykanyeva@gmail.com>
2022-12-23 18:57:48 -0600

    create Model mapper class

---
+++ src/main/java/com/cydeo/mapper/InvoiceProductMapper.java
@@ -0,0 +1,27 @@
+package com.cydeo.mapper;
+
+
+import com.cydeo.dto.InvoiceProductDTO;
+import com.cydeo.entity.InvoiceProduct;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InvoiceProductMapper {
+
+    private ModelMapper modelMapper;
+
+    public InvoiceProductMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+
+    public InvoiceProduct convertToEntity(InvoiceProductDTO<I> dto){
+        return modelMapper.map(dto, InvoiceProduct.class);
+    }
+
+    public InvoiceProductDTO<I> convertToDTO(InvoiceProduct entity){
+        return modelMapper.map(entity, InvoiceProductDTO.class);
+    }
+
+}
