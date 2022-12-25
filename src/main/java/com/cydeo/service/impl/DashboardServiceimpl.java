8c3554d1658d4414defdcd1377f866c3266e5017
entisar08 <eelmohbat@gmail.com>
2022-12-25 15:39:27 -0600

    [JDZH3-145] fix: Implemented DashboardService interface

--- src/main/java/com/cydeo/service/impl/DashboardServiceimpl.java
+++ src/main/java/com/cydeo/service/impl/DashboardServiceimpl.java
@@ -1,9 +1,8 @@
 package com.cydeo.service.impl;
 
 
-import com.cydeo.service.DashboardService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class DashboardServiceimpl implements DashboardService {
+public class DashboardServiceimpl {
 }
