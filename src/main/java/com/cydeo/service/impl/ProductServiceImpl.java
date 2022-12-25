b7f16173454f99786985504a2e4add6303e01db9
entisar08 <eelmohbat@gmail.com>
2022-12-24 21:17:49 -0600

    [JDZH3-77] fix: Implemented Product Service interface

--- src/main/java/com/cydeo/service/impl/ProductServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProductServiceImpl.java
@@ -1,8 +1,17 @@
 package com.cydeo.service.impl;
 
 
+import com.cydeo.repository.ProductRepository;
+import com.cydeo.service.ProductService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductService {
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    private final ProductRepository productRepository;
+
 }
