1fb6a0f4d920c78649d08a190626eb0aec94c43b
Adelina <adelina.abdykanyeva@gmail.com>
2022-12-24 23:05:42 -0600

    apply changes

--- src/main/java/com/cydeo/SparkleApplication.java
+++ src/main/java/com/cydeo/SparkleApplication.java
@@ -1,6 +1,7 @@
 package com.cydeo;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,5 +18,4 @@ public class SparkleApplication {
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
-
 }
