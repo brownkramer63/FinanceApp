2c58cb6f4097a8d2decf5f102cb60e9657f4f629
Kadifa <Kudickadifa@gmail.com>
2022-12-25 15:47:23 -0600

    [JDZH3-98] feat : Add CategoryService

--- src/main/java/com/cydeo/service/CategoryService.java
+++ src/main/java/com/cydeo/service/CategoryService.java
@@ -5,7 +5,7 @@ import com.cydeo.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface CategoryService{
 
 
