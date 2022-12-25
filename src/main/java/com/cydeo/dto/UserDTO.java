8c1e0bead3d5cee98473d772874349dc41c78af9
Violetta <violetta.v.queen@gmail.com>
2022-12-24 19:27:42 -0700

    [JDZH3 -74] style: commented out parts of code that has dependency

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -1,12 +1,12 @@
 package com.cydeo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Getter
+@Setter
 public class UserDTO {
 
     private Long id;
@@ -20,83 +20,5 @@ public class UserDTO {
     //private CompanyDTO company;
     private boolean isOnlyAdmin;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassWord() {
-        return passWord;
-    }
-
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public RoleDTO getRole() {
-        return role;
-    }
-
-    public void setRole(RoleDTO role) {
-        this.role = role;
-    }
-//
-//    public CompanyDTO getCompany() {
-//        return company;
-//    }
-//
-//    public void setCompany(CompanyDTO company) {
-//        this.company = company;
-//    }
-
-    public boolean isOnlyAdmin() {
-        return isOnlyAdmin;
-    }
-
-    public void setOnlyAdmin(boolean onlyAdmin) {
-        this.isOnlyAdmin = onlyAdmin;
-    }
 }
