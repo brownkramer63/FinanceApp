51a85f3be2d5bdffbf4936f17181bd4f1e51f8c8
Kadifa <Kudickadifa@gmail.com>
2022-12-25 22:22:46 -0600

    [JDZH3-106] feat : create new category

--- src/main/java/com/cydeo/controller/CategoryController.java
+++ src/main/java/com/cydeo/controller/CategoryController.java
@@ -1,10 +1,57 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.CategoryDTO;
+import com.cydeo.entity.Category;
+import com.cydeo.mapper.MapperUtil;
+import com.cydeo.repository.CategoryRepository;
+import com.cydeo.service.CategoryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/categories")
+@RequestMapping("/category")
 public class CategoryController {
+
+    public CategoryController(CategoryService categoryService, MapperUtil mapperUtil) {
+        this.categoryService = categoryService;
+        this.mapperUtil = mapperUtil;
+    }
+
+    private final CategoryService categoryService;
+    private final MapperUtil mapperUtil;
+
+
+
+
+
+    @GetMapping("/create")
+    public String createCategory(Model model){
+
+        model.addAttribute("newCategory", new CategoryDTO());
+
+        return "/category/category-create";
+    }
+
+    @PostMapping("/create")
+    public String insertCategory(@ModelAttribute("newCategory") CategoryDTO categoryDTO,  Model model){
+
+        categoryService.save(categoryDTO);
+
+        model.addAttribute("category",categoryService.listAllCategory());
+
+
+
+        return "/category/category-list";
+    }
+
+
+
+
+
+
 
 }
