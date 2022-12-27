62c6cb798e09231aac1fa88fde1a6f2115da24f8
Kadifa <Kudickadifa@gmail.com>
2022-12-26 22:30:42 -0600

    [JDZH3-99] feat : add categoryServiceImpl

--- src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
@@ -7,6 +7,10 @@ import com.cydeo.repository.CategoryRepository;
 import com.cydeo.service.CategoryService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
@@ -25,5 +29,46 @@ public class CategoryServiceImpl implements CategoryService {
 
         Category category = categoryRepository.findById(Id).get();
         return  mapperUtil.convert(category , new CategoryDTO());
+    }
+
+    @Override
+    public void save(CategoryDTO categoryDTO) {
+        Category category = mapperUtil.convert(categoryDTO , new Category());
+        categoryRepository.save(category);
+
+    }
+
+    @Override
+    public void update(CategoryDTO categoryDTO) {//this is find categoryDTO from DB
+
+        Optional<Category> category = categoryRepository.findById(categoryDTO.getId());
+              //convert DTO to entity
+         Category convertedCategory = mapperUtil.convert(categoryDTO, new Category());
+         convertedCategory.setId(category.get().getId());
+         categoryRepository.save(convertedCategory);
+
+
+
+
+    }
+
+    @Override
+    public void delete(Long Id) {
+        Optional<Category> foundCategory=categoryRepository.findById(Id);
+        if(foundCategory.isPresent()){
+            foundCategory.get().setIsDeleted(true);
+        }
+        categoryRepository.save(foundCategory.get());
+
+
+
+    }
+
+    @Override
+    public List<CategoryDTO> listAllCategory() {
+            List<Category> listCategory= categoryRepository.findAll();
+             return listCategory.stream().map(category-> mapperUtil.convert(category, new CategoryDTO()))
+                     .collect(Collectors.toList());
+
     }
 }
