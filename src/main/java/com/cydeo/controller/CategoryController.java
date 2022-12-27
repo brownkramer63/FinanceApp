0cf8396eeb6eea6397d615425fb50e267e5887c7
Kadifa <Kudickadifa@gmail.com>
2022-12-26 22:24:25 -0600

    [JDZH3-102] feat : add categoryController

--- src/main/java/com/cydeo/controller/CategoryController.java
+++ src/main/java/com/cydeo/controller/CategoryController.java
@@ -1,16 +1,15 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.CategoryDTO;
+import com.cydeo.entity.Address;
 import com.cydeo.entity.Category;
 import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.CategoryRepository;
 import com.cydeo.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/category")
@@ -36,16 +35,54 @@ public class CategoryController {
         return "/category/category-create";
     }
 
-    @PostMapping("/create")
-    public String insertCategory(@ModelAttribute("newCategory") CategoryDTO categoryDTO,  Model model){
 
+    @PostMapping("/create")
+    public String insertCategory(@ModelAttribute("newCategory") CategoryDTO categoryDTO){
         categoryService.save(categoryDTO);
 
+        return "redirect:/category-list";
+
+    }
+
+    @GetMapping("/list")
+    public String listCategory(Model model){
         model.addAttribute("category",categoryService.listAllCategory());
 
+        return "/category/category-list";
+    }
 
+
+
+    @GetMapping("/category/update/{id}")
+    public String updateCategory(@PathVariable("id") Long id, Model model) {
+
+        model.addAttribute("category", categoryService.findById(id));
 
         return "/category/category-list";
+
+    }
+
+    @PostMapping("/category/list")
+    public String updateCategory(@ModelAttribute("newCategory") CategoryDTO categoryDTO) {
+
+        categoryService.update(categoryDTO);
+
+        return "/category/category-list";
+
+    }
+
+    @GetMapping("/categories/delete/{id}")
+    public String deleteCategory(@PathVariable ("id") Long id){
+        categoryService.delete(id);
+        return "redirect:/category-list";
+
+    }
+
+
+
+
+
+
     }
 
 
@@ -54,4 +91,3 @@ public class CategoryController {
 
 
 
-}
