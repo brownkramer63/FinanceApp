80b65637064483e60f1a833280e741bfb56f24e8
Kramer Brown <brownkramer63@gmail.com>
2022-12-27 15:40:45 -0600

    JDZH3-87/10/12 revised with classes added

---
+++ src/main/java/com/cydeo/entity/ClientVendor.java
@@ -0,0 +1,28 @@
+package com.cydeo.entity;
+
+
+import com.cydeo.enums.ClientVendorType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "clients_vendors")
+public class ClientVendor extends BaseEntity{
+
+    private String clientVendorName;
+    private String phone;
+    private String website;
+    @Enumerated(EnumType.STRING)
+    private ClientVendorType clientVendorType;
+    @OneToOne
+    private Address address;
+    @ManyToOne
+    private Company company;
+
+}
