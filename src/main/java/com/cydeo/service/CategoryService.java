3365036afc964803671647751d3be991ef2bf85e
Kadifa <Kudickadifa@gmail.com>
2022-12-26 22:30:08 -0600

    [JDZH3-98] feat : add categoryService

--- src/main/java/com/cydeo/service/CategoryService.java
+++ src/main/java/com/cydeo/service/CategoryService.java
@@ -5,12 +5,19 @@ import com.cydeo.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 public interface CategoryService{
 
 
 
     CategoryDTO findById(Long Id);
+    void save(CategoryDTO categoryDTO);
+    void update(CategoryDTO categoryDTO);
+    void delete(Long Id);
+    List<CategoryDTO> listAllCategory();
+
 
 
 
