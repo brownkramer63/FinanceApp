f15acad72cb43092b91cf9f747a7edc8f3a484e6
Adelina <adelina.abdykanyeva@gmail.com>
2022-12-27 09:45:42 -0600

    create Invoice Controller

--- src/main/java/com/cydeo/controller/InvoiceController.java
+++ src/main/java/com/cydeo/controller/InvoiceController.java
@@ -14,26 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class InvoiceController {
 
 
-    private final InvoiceService invoiceService;
-
-
-    public InvoiceController(InvoiceService invoiceService) {
-        this.invoiceService = invoiceService;
-    }
-
-    @GetMapping("/salesInvoices/list")
-    public String createSalesInvoice(Model model){
-        model.addAttribute("invoices", new InvoiceDTO());
-
-        return "/invoice/sales-invoice-list";
-    }
-
-    @GetMapping("/delete/{id}")
-    public String deleteSalesInvoice(@PathVariable("id") Long id){
-        invoiceService.delete(id);
-        return "redirect:/invoice/salesInvoices/list";
-
-    }
 
 
 
