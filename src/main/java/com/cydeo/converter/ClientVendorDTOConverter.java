b876966dbbf233520c741d27c6f43e06a6b8c5e9
Kramer Brown <brownkramer63@gmail.com>
2022-12-28 12:05:29 -0600

    JDZH3-88/89/90 clientvendorDTO class added converter class added as well as serviceimpl class added

---
+++ src/main/java/com/cydeo/converter/ClientVendorDTOConverter.java
@@ -0,0 +1,22 @@
+package com.cydeo.converter;
+
+import com.cydeo.dto.ClientVendorDTO;
+import com.cydeo.service.ClientVendorService;
+import org.springframework.core.convert.converter.Converter;
+
+public class ClientVendorDTOConverter implements Converter<String, ClientVendorDTO> {
+
+    private final ClientVendorService clientVendorService;
+
+    public ClientVendorDTOConverter(ClientVendorService clientVendorService) {
+        this.clientVendorService = clientVendorService;
+    }
+
+    @Override
+    public ClientVendorDTO convert(String source) {
+       if (source==null || source.equals("")){
+           return null;
+       }
+       return clientVendorService.findById(Long.parseLong(source));
+    }
+}
--- src/main/java/com/cydeo/service/ClientVendorService.java
+++ src/main/java/com/cydeo/service/ClientVendorService.java
@@ -1,7 +1,16 @@
 package com.cydeo.service;
 
 
+import com.cydeo.dto.ClientVendorDTO;
 
 public interface ClientVendorService {
+
+    ClientVendorDTO findById(Long id);
+
+    public void save(ClientVendorDTO clientVendorDTO);
+
+    void delete(Long id);
+
+    void update(ClientVendorDTO clientVendorDTO);
 
 }
