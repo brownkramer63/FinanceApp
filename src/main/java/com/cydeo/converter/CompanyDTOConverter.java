428e106243ce6569fcef0df5e3932e0554159bb6
nihano <nioztekin@gmail.com>
2023-01-01 01:50:59 +0300

    [JDZH3-68] update: adds annotations to CompanyDTOConverter

--- src/main/java/com/cydeo/converter/CompanyDTOConverter.java
+++ src/main/java/com/cydeo/converter/CompanyDTOConverter.java
@@ -2,14 +2,19 @@ package com.cydeo.converter;
 
 import com.cydeo.dto.CompanyDTO;
 import com.cydeo.service.CompanyService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
+@ConfigurationPropertiesBinding
 public class CompanyDTOConverter implements Converter<String, CompanyDTO> {
 
     private final CompanyService companyService;
 
 
-    public CompanyDTOConverter(CompanyService companyService) {
+    public CompanyDTOConverter(@Lazy CompanyService companyService) {
         this.companyService = companyService;
     }
 
