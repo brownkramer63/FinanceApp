ff360472284bf58a1cf05f11527ccd7f2b2a9691
Violetta <violetta.v.queen@gmail.com>
2022-12-31 13:00:58 -0700

    [JDZH3 -71-1] feat: findAllByLoggedInUser added in UserService

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -12,4 +12,5 @@ public interface UserService {
     UserDTO update (UserDTO userDTO);
     void delete(Long id);
     UserDTO findByUsername(String username);
+    UserDTO findAllByLoggedInUser();
 }
