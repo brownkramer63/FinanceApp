a8c5c3695dc6558c4e9c9b297b651caa26496f43
Violetta <104214760+violettaqueen@users.noreply.github.com>
2022-12-31 07:51:07 -0700

    [JDZH-71] dashboard controller return html page (#43) update : user controller update method bugfix
    * [JDZH3 -71] feat: added insertUser @PostMapping userCreatePage changed to @GetMapping
    
    * [JDZH3 -71] fix: @ModelAttirbute added
    
    * [JDZH3 -71] fix: dash added
    
    * [JDZH3 -71] fix: deleted SecurityService bean
    
    * [JDZH3 -71] fix: userDTOConverter fixed
    
    * [JDZH3 -71] fix: UserController
     fixed
    
    * [JDZH3 -71] fix: UserController
     fixed
    
    * [JDZH3 -71] fix: dashboard
    
    * [JDZH3 -71] fix: dashboard fixes
    
    * [JDZH3 -71] fix: dashboard fixes
    
    * [JDZH3 -71] fix: dashboard fixes

--- src/main/java/com/cydeo/controller/DashboardController.java
+++ src/main/java/com/cydeo/controller/DashboardController.java
@@ -1,11 +1,22 @@
 package com.cydeo.controller;
 
+import com.cydeo.service.DashboardService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/dashboard")
+@RequestMapping()
 
 public class DashboardController {
 
+    @GetMapping("/dashboard")
+    public String navigateToDashboard(Model model){
+
+        return "dashboard";
+
+    }
+
 }
+
