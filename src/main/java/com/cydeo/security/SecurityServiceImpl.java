398a2151f3f2ca86d8bcd8faeae0aab895a43e9c
Violetta <violetta.v.queen@gmail.com>
2023-01-01 11:13:50 -0700

    [JDZH3 -71-1] fix: @Lazy added to SecurityServiceImpl

--- src/main/java/com/cydeo/security/SecurityServiceImpl.java
+++ src/main/java/com/cydeo/security/SecurityServiceImpl.java
@@ -4,18 +4,22 @@ import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.User;
 import com.cydeo.repository.UserRepository;
 import com.cydeo.service.UserService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
 @Service
 public class SecurityServiceImpl implements SecurityService {
 
     private final UserService userService;
     private final UserRepository userRepository;
+
+    public SecurityServiceImpl(@Lazy UserService userService, UserRepository userRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDTO getLoggedInUser() {
