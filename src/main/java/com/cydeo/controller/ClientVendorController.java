e4f2fbb55de809ab2066d31d0fac4d064cae9a19
Kramer Brown <brownkramer63@gmail.com>
2023-01-02 12:41:26 -0600

    JDZH3-92 completed update methods added

--- src/main/java/com/cydeo/controller/ClientVendorController.java
+++ src/main/java/com/cydeo/controller/ClientVendorController.java
@@ -2,15 +2,13 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.ClientVendorDTO;
 import com.cydeo.entity.ClientVendor;
+import com.cydeo.enums.ClientVendorType;
 import com.cydeo.mapper.MapperUtil;
 import com.cydeo.service.ClientVendorService;
 import org.apache.catalina.LifecycleState;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,13 +29,14 @@ public class ClientVendorController {
         List<ClientVendorDTO> clientVendors= clientVendorService.listAllClientVendors();
         model.addAttribute("clientvendors",clientVendors);
         return "redirect:/clientVendors/list";
-  //check logic here
+  //should be good
     }
     @PostMapping("/create")
     public String createClientVendor(Model model){
 
         model.addAttribute("clientVendor", new ClientVendorDTO());
-//        need to figure out the logic to add here
+        model.addAttribute("country list", List.of("USA","Canada","Germany") );
+        model.addAttribute("clientVendorTypes", ClientVendorType.values());
 
 
         return "redirect:/clientVendors/create";
@@ -45,9 +44,25 @@ public class ClientVendorController {
     @PostMapping("/create")
     public String insertClientVendor(@ModelAttribute("clientVendor") ClientVendorDTO clientVendorDTO, Model model){
 
-        //check logic here
         clientVendorService.save(clientVendorDTO);
         return "redirect:/clientVendors/create";
+    }
+
+    @GetMapping("/update/{id}")
+    public String editClientVendor(@PathVariable("clientVendorId") Long clientVendorId, Model model){
+
+        model.addAttribute("clientVendor", clientVendorService.findById(clientVendorId));
+        model.addAttribute("country list", List.of("USA","Canada","Germany") );
+        model.addAttribute("clientVendorTypes", ClientVendorType.values());
+    return "clientVendor/clientVendor-update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String editClientVendor(@ModelAttribute("clientVendor") ClientVendorDTO clientVendorDTO, @PathVariable("clientVendorId") Long clientVendorId,Model model){
+
+        clientVendorService.save(clientVendorDTO);
+        return "redirect:/clientVendors/list";
+
     }
 
 
--- src/main/java/com/cydeo/service/impl/ClientVendorServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ClientVendorServiceImpl.java
@@ -4,19 +4,28 @@ import com.cydeo.dto.ClientVendorDTO;
 import com.cydeo.entity.ClientVendor;
 import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.ClientVendorRepository;
+import com.cydeo.repository.UserRepository;
+import com.cydeo.security.SecurityServiceImpl;
 import com.cydeo.service.ClientVendorService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 public class ClientVendorServiceImpl implements ClientVendorService {
 
     private final ClientVendorRepository clientVendorRepository;
+    private final UserRepository userRepository;
+    private final SecurityServiceImpl securityService;
     private final MapperUtil mapperUtil;
 
-    public ClientVendorServiceImpl(ClientVendorRepository clientVendorRepository, MapperUtil mapperUtil) {
+    public ClientVendorServiceImpl(ClientVendorRepository clientVendorRepository, UserRepository userRepository, SecurityServiceImpl securityService, MapperUtil mapperUtil) {
         this.clientVendorRepository = clientVendorRepository;
+        this.userRepository = userRepository;
+        this.securityService = securityService;
         this.mapperUtil = mapperUtil;
     }
 
@@ -50,12 +59,16 @@ public class ClientVendorServiceImpl implements ClientVendorService {
 
     @Override
     public List<ClientVendorDTO> listAllClientVendors() {
-        return null;
+        return clientVendorRepository.findAll(Sort.by("clientVendorType")).stream().filter(clientVendor ->
+                clientVendor.getCompany().getId().equals(securityService.getLoggedInUser().getCompany().getId())).map(
+                        clientVendor -> mapperUtil.convert(clientVendor, new ClientVendorDTO())).collect(Collectors.toList());
+
     }
 
     @Override
     public List<ClientVendor> findAllByClientVendorName(String name) {
-        return null;
+       return clientVendorRepository.findAll().stream().filter(clientVendor -> clientVendor.getClientVendorName().equals(name)).collect(Collectors.toList());
+
     }
 
 
