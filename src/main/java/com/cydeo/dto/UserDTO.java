20af651c0ecc740fc83d897c436a17e56e358bc6
Violetta <violetta.v.queen@gmail.com>
2023-01-02 08:16:23 -0700

    [JDZH3 -71-1] feat: password setter and getter added and checkConfirmPassword

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -2,13 +2,9 @@ package com.cydeo.dto;
 
 import lombok.*;
 import javax.validation.constraints.*;
-
-
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@Getter
-@Setter
 public class UserDTO {
 
     private Long id;
@@ -33,5 +29,91 @@ public class UserDTO {
     private CompanyDTO company;
     private boolean isOnlyAdmin;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+        checkConfirmPassword();
+    }
+    public void checkConfirmPassword(){
+        if (this.password == null || this.confirmPassword == null){
+            return;
+        } else if (!this.password.equals(this.confirmPassword)) {
+            this.confirmPassword=null;
+        }
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public RoleDTO getRole() {
+        return role;
+    }
+
+    public void setRole(RoleDTO role) {
+        this.role = role;
+    }
+
+    public CompanyDTO getCompany() {
+        return company;
+    }
+
+    public void setCompany(CompanyDTO company) {
+        this.company = company;
+    }
+
+    public boolean isOnlyAdmin() {
+        return isOnlyAdmin;
+    }
+
+    public void setOnlyAdmin(boolean onlyAdmin) {
+        isOnlyAdmin = onlyAdmin;
+    }
 }
