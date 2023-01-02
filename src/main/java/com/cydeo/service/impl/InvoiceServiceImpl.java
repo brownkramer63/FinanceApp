00ecbf0b8967c91b72cd3173d06f4e9864af38ac
Adelina Abdykanyeva <adelina.abdykanyeva@gmail.com>
2023-01-02 09:45:15 -0600

    Jdzh3 128 (#51)
    * [JDZH3-128]feat: delete, update, list purchase invoices ready to review
    
    * [JDZH3-128]feat: delete, update, list purchase invoices ready to review
    
    * [JDZH3-128]feat: delete, update, list purchase invoices ready to review
    
    * Revert "[JDZH3-128]feat: delete, update, list purchase invoices ready to review"
    
    This reverts commit e226314854f711e4efb4ed43244b9d64fea4d62e.
    
    * Revert "Revert "[JDZH3-128]feat: delete, update, list purchase invoices ready to review""
    
    This reverts commit 0d005a2d00c85f22be096f9217ee8ebbe6aaef64.
    
    * [JDZH3-128]feat: SalesInvoiceController complete

--- src/main/java/com/cydeo/service/impl/InvoiceServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/InvoiceServiceImpl.java
@@ -1,22 +1,44 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.CompanyDTO;
 import com.cydeo.dto.InvoiceDTO;
+import com.cydeo.dto.InvoiceProductDTO;
+import com.cydeo.entity.Company;
+import com.cydeo.entity.Invoice;
+import com.cydeo.enums.InvoiceStatus;
+import com.cydeo.enums.InvoiceType;
 import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.InvoiceRepository;
+import com.cydeo.service.CompanyService;
+import com.cydeo.service.InvoiceProductService;
 import com.cydeo.service.InvoiceService;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class InvoiceServiceImpl implements InvoiceService {
 
     private final InvoiceRepository invoiceRepository;
     private final MapperUtil mapperUtil;
+    private final InvoiceProductService invoiceProductService;
+
+    private final CompanyService companyService;
+    private final Invoice invoice;
 
 
-    public InvoiceServiceImpl(InvoiceRepository invoiceRepository, MapperUtil mapperUtil) {
+    public InvoiceServiceImpl(InvoiceRepository invoiceRepository, MapperUtil mapperUtil, InvoiceProductService invoiceProductService, CompanyService companyService, Invoice invoice) {
         this.invoiceRepository = invoiceRepository;
 
         this.mapperUtil = mapperUtil;
+        this.invoiceProductService = invoiceProductService;
+        this.companyService = companyService;
+        this.invoice = invoice;
     }
 
     @Override
@@ -28,7 +50,128 @@ public class InvoiceServiceImpl implements InvoiceService {
 
     @Override
     public void delete(Long id) {
-        invoiceRepository.deleteById(id);
+        Optional<Invoice> getInvoice = invoiceRepository.findById(id);
+
+        if(getInvoice.isPresent()){
+            getInvoice.get().setIsDeleted(true);
+            invoiceRepository.save(getInvoice.get());
+
+
+        }
+    }
+
+    @Override
+    public List<InvoiceDTO> listAllInvoice() {
+        return invoiceRepository.findAll().stream()
+                .map(invoice -> mapperUtil.convert(invoice, new InvoiceDTO()))
+                .collect(Collectors.toList());
 
     }
+
+
+
+    @Override
+    public void approve(Long id) {
+
+        Optional<Invoice> invoice = invoiceRepository.findById(id);
+
+        if(invoice.isPresent()){
+            invoice.get().setInvoiceStatus(InvoiceStatus.APPROVED);
+            invoiceRepository.save(invoice.get());
+        }
+    }
+
+    @Override
+    public List<InvoiceDTO> listAllInvoicesByType(InvoiceType invoiceType) {
+        //TODO @nihano add method to the companyService
+       // CompanyDTO companyDTO = companyService.findCompanyByUser();
+        CompanyDTO companyDTO = companyService.findById(1l);
+
+
+      List<Invoice> invoiceListByType = invoiceRepository.findAllByCompanyAndInvoiceTypeOrderByDateDesc(mapperUtil.convert(companyDTO, new Company()),invoiceType)
+              .stream().sorted(Comparator.comparing(Invoice::getInvoiceNo)).collect(Collectors.toList());
+
+      return invoiceDtoList(invoiceListByType);
+    }
+
+    @Override
+    public InvoiceDTO getNewPurchaseInvoice() {
+
+       InvoiceDTO invoiceDTO = new InvoiceDTO();
+       invoiceDTO.setDate(LocalDate.now());
+       invoiceDTO.setInvoiceStatus(InvoiceStatus.AWAITING_APPROVAL);
+       invoiceDTO.setInvoiceNumber(generateInvoiceNumber(InvoiceType.PURCHASE));
+
+       return invoiceDTO;
+
+    }
+
+
+    private String generateInvoiceNumber(InvoiceType invoiceType){
+        //TODO @nihano add method to the companyService
+
+       // CompanyDTO companyDTO = companyService.findCompanyByUser();
+        CompanyDTO companyDTO = companyService.findById(1l);
+        Company company = mapperUtil.convert(companyDTO, new Company());
+
+        Invoice invoice = invoiceRepository.findAllByCompanyAndInvoiceTypeOrderByDateDesc(company, invoiceType).stream()
+                .max(Comparator.comparing(Invoice::getInvoiceNo)).orElseThrow();
+
+        String invoiceNum = invoice.getInvoiceNo();
+        int res = Integer.parseInt(invoiceNum.substring(2)) + 1;
+        String numberPart = String.format("%03d", res);
+        String generatedNumber = invoiceNum.substring(0, 2) + numberPart;
+        return generatedNumber;
+    }
+
+
+    private List<InvoiceDTO> invoiceDtoList(List<Invoice> invoiceList){
+
+        List<InvoiceDTO> invoiceDTOList = invoiceList.stream()
+                .map(invoice ->mapperUtil.convert(invoice, new InvoiceDTO()))
+                .collect(Collectors.toList());
+
+        List<InvoiceDTO> dtoList = invoiceDTOList.stream()
+                .map(invoiceDTO -> {
+                    List<InvoiceProductDTO> invoiceProductDTOList = invoiceProductService.findByInvoiceProductId(invoiceDTO.getId());
+
+                    BigDecimal totalPrice = invoiceProductDTOList.stream().map(invoiceProductDTO ->
+                            invoiceProductDTO.getPrice().multiply(BigDecimal.valueOf(invoiceProductDTO.getQuantity()))).reduce(BigDecimal.ZERO, BigDecimal::add);
+                    invoiceDTO.setPrice(totalPrice);
+
+                    BigDecimal totalTax = invoiceProductDTOList.stream().map(invoiceProductDTO ->
+                            invoiceProductDTO.getPrice().multiply(BigDecimal.valueOf(invoiceProductDTO.getTax())).divide(BigDecimal.valueOf(100)))
+                            .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+                    invoiceDTO.setTax(totalTax.intValueExact());
+
+                    BigDecimal totalPriceWithTax = totalPrice.add(totalTax);
+                    invoiceDTO.setTotal(totalPriceWithTax);
+                    return invoiceDTO;
+                }
+                ).collect(Collectors.toList());
+
+        return dtoList;
+
+    }
+
+
+
+
+    @Override
+    public InvoiceDTO create(InvoiceDTO invoiceDTO, InvoiceType invoiceType) {
+
+        Company company = invoiceRepository.findAll().stream()
+                .findAny().get().getCompany();
+        invoiceDTO.setCompanyDTO(mapperUtil.convert(company, new CompanyDTO()));
+        invoiceDTO.setInvoiceType(invoiceType);
+        invoiceDTO.setInvoiceStatus(InvoiceStatus.AWAITING_APPROVAL);
+        invoiceRepository.save(mapperUtil.convert(invoiceDTO, new Invoice()));
+
+        return invoiceDTO;
+
+    }
+
+
+
 }
