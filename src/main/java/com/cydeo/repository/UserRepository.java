bfb585fae6384adc0406cf70c445f59da1822d27
Violetta <violetta.v.queen@gmail.com>
2023-01-02 08:19:36 -0700

    [JDZH3 -71-1] feat: some services added

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -12,5 +11,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findUserByUsernameAndIsDeleted(String username, Boolean deleted);
 
     Optional<User> findByUsername(String username);
-    List <User> findByCompanyId(Long id);
+    List <User> findAllByCompanyId(Long id);
+    List<User> findAllByCompany_IdAndRoleIsNotContaining(Long id, String description);
+    List<User> findAllByRole_DescriptionOrderByCompany(String description);
 }
