089d8ba0e2d8cc4748c180ca34e0839b5904950f
Kramer Brown <brownkramer63@gmail.com>
2023-01-03 15:12:24 -0600

    [JDZH3-94/96] updated: regex added as well as validations,update method updated

--- src/main/java/com/cydeo/controller/ClientVendorController.java
+++ src/main/java/com/cydeo/controller/ClientVendorController.java
@@ -78,7 +78,7 @@ public class ClientVendorController {
             return "clientVendor/clientVendor-update";
 
         }
-        clientVendorService.save(clientVendorDTO);
+        clientVendorService.update(clientVendorDTO);
         return "redirect:/clientVendors/list";
 
     }
