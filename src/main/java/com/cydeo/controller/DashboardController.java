5850557a16342c16cec11f02c7e93d0553958c61
entisar08 <eelmohbat@gmail.com>
2023-01-04 20:31:20 -0600

    DashBoard controller

--- src/main/java/com/cydeo/controller/DashboardController.java
+++ src/main/java/com/cydeo/controller/DashboardController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 @Controller
 @RequestMapping()
 
@@ -13,7 +16,11 @@ public class DashboardController {
 
     @GetMapping("/dashboard")
     public String navigateToDashboard(Model model){
-
+        Map<String,Integer> map=  new TreeMap<>();
+        map.put("totalCost",0);
+        map.put("totalSales",0);
+        map.put("profitLoss",0);
+        model.addAttribute("summaryNumbers",map);
         return "dashboard";
 
     }
