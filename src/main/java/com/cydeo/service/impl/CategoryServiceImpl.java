11706f5a76737fbcb00285805a12a95e867631d6
Kadifa <Kudickadifa@gmail.com>
2023-01-06 13:11:20 -0600

    Category List method update

--- src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
@@ -1,29 +1,41 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.CategoryDTO;
+import com.cydeo.dto.CompanyDTO;
 import com.cydeo.entity.Category;
+import com.cydeo.entity.Company;
 import com.cydeo.entity.Product;
 import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.CategoryRepository;
+import com.cydeo.repository.CompanyRepository;
 import com.cydeo.repository.ProductRepository;
 import com.cydeo.service.CategoryService;
+import com.cydeo.service.CompanyService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
     private final ProductRepository productRepository;
+    private final CompanyService companyService;
+
 
     private final MapperUtil mapperUtil;
 
-    public CategoryServiceImpl(CategoryRepository categoryRepository, ProductRepository productRepository, MapperUtil mapperUtil) {
+    public CategoryServiceImpl(CategoryRepository categoryRepository, ProductRepository productRepository, CompanyService companyService, MapperUtil mapperUtil) {
         this.categoryRepository = categoryRepository;
         this.productRepository = productRepository;
+        this.companyService = companyService;
         this.mapperUtil = mapperUtil;
     }
 
@@ -37,19 +49,43 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void save(CategoryDTO categoryDTO) {
-        Category category = mapperUtil.convert(categoryDTO , new Category());
-        categoryRepository.save(category);
 
-    }
+            CompanyDTO companyDTO = companyService.getCompanyByLoggedInUser();
+            log.info("cat description : " + companyDTO.getId());
+            categoryDTO.setCompany(companyDTO);
+            log.info("cat  company : " + categoryDTO.getCompany().getId());
+            Category category = mapperUtil.convert(categoryDTO , new Category());
+            Company company = mapperUtil.convert(companyDTO, new Company());
+            List<Category> list = categoryRepository.findAllByIsDeletedAndCompany(false,company);
+            List<String> descList=list.stream().map(Category::getDescription).collect(Collectors.toList());
+            // log.info(" cat description : " + descList);
+            if(descList.contains(category.getDescription())){
+                return;
+            }else{
+                categoryRepository.save(category);
+            }
+
+
+        }
+
+
+
 
     @Override
     public void update(CategoryDTO categoryDTO) {//this is find categoryDTO from DB
 
-        Optional<Category> category = categoryRepository.findById(categoryDTO.getId());
-              //convert DTO to entity
-         Category convertedCategory = mapperUtil.convert(categoryDTO, new Category());
-         convertedCategory.setId(category.get().getId());
-         categoryRepository.save(convertedCategory);
+        CompanyDTO companyDTO = companyService.getCompanyByLoggedInUser();
+        categoryDTO.setCompany(companyDTO);
+        //Company company = mapperUtil.convert(companyDTO, new Company());
+
+        //Category category = categoryRepository.findByIdAndIsDeletedAndCompany(categoryDTO.getId(),false,company);
+        //convert DTO to entity
+        Category convertedCategory = mapperUtil.convert(categoryDTO, new Category());
+        convertedCategory.setId(categoryDTO.getId());
+        categoryRepository.save(convertedCategory);
+
+
+
 
 
     }
@@ -72,10 +108,19 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<CategoryDTO> listAllCategory() {
-            List<Category> listCategory= categoryRepository.findAllByIsDeleted(false);
-             return listCategory.stream().map(category-> mapperUtil.convert(category, new CategoryDTO()))
-                     .collect(Collectors.toList());
+        CompanyDTO companyDTO = companyService.getCompanyByLoggedInUser();
+        Company company = mapperUtil.convert(companyDTO, new Company());
 
+        List<Category> listCategory= categoryRepository.findAllByIsDeletedAndCompany(false, company);
+        List<CategoryDTO> categoryDTOList = listCategory.stream().map(category-> mapperUtil.convert(category, new CategoryDTO()))
+                   .collect(Collectors.toList());
+                for (int i = 0, j=0; i < categoryDTOList.size() ; i++ ,j++) {
+                    if(!(checkIfCategoryCanBeDeleted(listCategory.get(i)))){
+                        (categoryDTOList.get(i)).setHasProduct(true);
+                    }
+
+        }
+                return categoryDTOList;
     }
 
     private boolean checkIfCategoryCanBeDeleted(Category category) {
