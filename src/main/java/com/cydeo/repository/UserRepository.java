f8e7d4328b292f304447df3b6397d6387eae657f
Violetta <violetta.v.queen@gmail.com>
2023-01-05 17:11:44 -0700

    [JDZH3 -74] feat:userRepository query added

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -11,9 +11,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findById(Long id);
     User findUserByUsernameAndIsDeleted(String username, Boolean deleted);
-    Optional<User> findByUsername(String username);public
+    //User findUserByUsername(String username);
+    Optional<User> findByUsername(String username);
     List <User> findAllByCompanyId(Long id);
     @Query("select u from User u join Role r on u.role.id = r.id where u.role.description = ?1 order by u.company.title, u.role.description")
     List<User> getAllByOrderByCompanyAndRole(String description);
+    @Query("select u from User u order by u.company.title, u.role.description")
+    List<User> getAllByCompanyAndRole();
+    User findUserByCompanyId(Long id);
 
 }
