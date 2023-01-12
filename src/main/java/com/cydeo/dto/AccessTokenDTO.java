19929a625c82811d7a8e16fbd191b59839171c19
Kramer Brown <brownkramer63@gmail.com>
2023-01-12 16:05:08 -0600

    [JDZH3-142] consumingAPI works with changes to respected classes

--- src/main/java/com/cydeo/dto/AccessTokenDTO.java
+++ src/main/java/com/cydeo/dto/AccessTokenDTO.java
@@ -1,15 +1,46 @@
 package com.cydeo.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@Builder
-@Getter
-@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "auth_token"
+})
+@Generated("jsonschema2pojo")
 public class AccessTokenDTO {
 
-    private String auth_token;
+    @JsonProperty("auth_token")
+    private String authToken;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
+    @JsonProperty("auth_token")
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    @JsonProperty("auth_token")
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
 
 }
+
--- src/main/java/com/cydeo/service/impl/AddressServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/AddressServiceImpl.java
@@ -3,17 +3,25 @@ package com.cydeo.service.impl;
 import com.cydeo.client.ConsumeCountriesAPI;
 import com.cydeo.dto.AccessTokenDTO;
 import com.cydeo.dto.AddressDTO;
+import com.cydeo.dto.CountryListDTO;
 import com.cydeo.repository.AddressRepository;
 import com.cydeo.service.AddressService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressServiceImpl implements AddressService {
     private final AddressRepository addressRepository;
     private final ConsumeCountriesAPI consumeCountriesAPI;
 
+    @Value("${user_email}")
+    private String user_email;
+
+    @Value("${api_token}")
+    private String api_token;
 
 
     public AddressServiceImpl(AddressRepository addressRepository, ConsumeCountriesAPI consumeCountriesAPI) {
@@ -21,15 +29,16 @@ public class AddressServiceImpl implements AddressService {
         this.consumeCountriesAPI = consumeCountriesAPI;
     }
 
-
     @Override
     public List<String> getListOfCountries() {
-       AccessTokenDTO accessToken= consumeCountriesAPI.getAccessToken();
-        return consumeCountriesAPI.getListOfCountries();
+        List<CountryListDTO> countryListDTOS= consumeCountriesAPI.getListOfCountries(getAuthToken());
+        return countryListDTOS.stream().map(CountryListDTO::getCountryName).collect(Collectors.toList());
     }
 
-    @Override
-    public AccessTokenDTO getAccessToken() {
-        return consumeCountriesAPI.getAccessToken();
+
+    private String getAuthToken(){
+        AccessTokenDTO accessTokenDTO= consumeCountriesAPI.getAccessToken(api_token,user_email);
+        return "Bearer "+accessTokenDTO.getAuthToken();
     }
+
 }
