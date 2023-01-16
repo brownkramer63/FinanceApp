8d862806e7b70aba7aff9345288345fd703809df
nihano <nioztekin@gmail.com>
2023-01-16 22:05:06 +0300

    [JDZH3-179] LoggingAspect

--- src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -6,10 +6,8 @@ import com.cydeo.service.CompanyService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -19,5 +17,35 @@ import java.util.Arrays;
 @Slf4j
 @AllArgsConstructor
 public class LoggingAspect {
+
+    private String getUsername() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    @Pointcut("execution(* com.cydeo.controller.*.*(..))")
+    public void anyControllerPC() {}
+
+    @Before("anyControllerPC()")
+    public void beforeAnyControllerPC(JoinPoint joinPoint){
+        log.info("Before -> Method: {}, User: {}"
+                , joinPoint.getSignature().toShortString()
+                , getUsername());
+    }
+
+    @AfterReturning(pointcut = "anyControllerPC()", returning = "results")
+    public void afterReturningAnyController(JoinPoint joinPoint, Object results) {
+        log.info("After Returning -> Method: {}, User: {}, Results: {}"
+                , joinPoint.getSignature().toShortString()
+                , getUsername()
+                , results.toString());
+    }
+
+    @AfterThrowing(pointcut = "anyControllerPC()", throwing = "exception")
+    public void afterThrowingAnyController(JoinPoint joinPoint, Exception exception) {
+        log.info("After Throwing -> Method: {}, User: {}, Results: {}"
+                , joinPoint.getSignature().toShortString()
+                , getUsername()
+                , exception.getMessage());
+    }
 
 }
